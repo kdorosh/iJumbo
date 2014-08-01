d450d54ad13ef0dea930dee2c4368f685406d319
Amadou Crookes <ascrookes@gmail.com>
2014-08-01 19:12:39 -0400

    Initial Commit

---
+++ iJumbo/IJAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  IJAppDelegate.h
+//  iJumbo
+//
+//  Created by Amadou Crookes on 8/1/14.
+//  Copyright (c) 2014 Amadou.It. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ iJumbo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iJumbo
+//
+//  Created by Amadou Crookes on 8/1/14.
+//  Copyright (c) 2014 Amadou.It. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IJAppDelegate class]));
+    }
+}
