381e03de439fee6a0084014c642f7ff1bee76455
Amadou Crookes <ascrookes@gmail.com>
2014-08-20 02:01:57 -0400

    event model description alt name

---
+++ iJumbo/UI/ViewControllers/IJEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  IJEventViewController.h
+//  iJumbo
+//
+//  Created by Amadou Crookes on 8/20/14.
+//  Copyright (c) 2014 Amadou.It. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IJEventViewController : UIViewController
+
+@end
---
+++ iJumbo/UI/ViewControllers/IJEventViewController.m
@@ -0,0 +1,49 @@
+//
+//  IJEventViewController.m
+//  iJumbo
+//
+//  Created by Amadou Crookes on 8/20/14.
+//  Copyright (c) 2014 Amadou.It. All rights reserved.
+//
+
+#import "IJEventViewController.h"
+
+@interface IJEventViewController ()
+
+@end
+
+@implementation IJEventViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
